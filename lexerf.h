3e03ea9aaed3fa5908276141c123c04224559998
David Woolsey <woolsey2316@gmail.com>
2024-07-08 20:59:57 +1000

    addition and subtraction

--- lexerf.h
+++ lexerf.h
@@ -1,10 +1,12 @@
 #ifndef LEXER_H_
-#define LEXER_H
+#define LEXER_H_
 
 typedef enum {
+  BEGINNING,
   INT,
   KEYWORD,
   SEPARATOR,
+  OPERATOR,
   END_OF_TOKENS,
 } TokenType;
 
@@ -17,7 +19,7 @@ typedef struct {
 void print_token(Token token);
 Token *generate_number(char *current, int *current_index);
 Token *generate_keyword(char *current, int *current_index);
-Token *generate_separator(char *current, int *current_index);
+Token *generate_separator_or_operator(char *current, int *current_index, TokenType type);
 Token *lexer(FILE *file);
 
 #endif
